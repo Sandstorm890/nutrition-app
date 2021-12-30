0788209c52938b690528d439e887c7404d5d0c9d
zaynegoering <zaynegoering@gmail.com>
2021-12-29 16:52:27 -0800

    change url to local test data for fetch

--- src/actions/foodActions.js
+++ src/actions/foodActions.js
@@ -1,12 +1,12 @@
-const url = 'https://api.nal.usda.gov/fdc/v1/foods/search?api_key=QhydPZDu2L1Q1Faaw3ZO6bJ53WEu66LdBHIMfdDF&query='
-const url_all = 'https://api.nal.usda.gov/fdc/v1/foods/list?api_key=QhydPZDu2L1Q1Faaw3ZO6bJ53WEu66LdBHIMfdDF'
-
+// const url = 'https://api.nal.usda.gov/fdc/v1/foods/search?api_key=QhydPZDu2L1Q1Faaw3ZO6bJ53WEu66LdBHIMfdDF&query='
+// const url_all = 'https://api.nal.usda.gov/fdc/v1/foods/list?api_key=QhydPZDu2L1Q1Faaw3ZO6bJ53WEu66LdBHIMfdDF'
+// import FoodData from '../resources/FoodData.json'
 export const setFoods = (foods) => ({type: 'GOT_FOODS', payload: foods})
 
 export const getFoods = () => {
 
     return (dispatch) => {
-        fetch(url_all)
+        fetch('FoodData_Branded_Test.json')
         .then(r => r.json())
         .then(json => {
             dispatch(setFoods(json))
