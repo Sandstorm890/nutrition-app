680cc95370826117f24ba75d6db1498daa453905
zaynegoering <zaynegoering@gmail.com>
2022-12-10 15:30:15 -0800

    properly display food names and carb info

--- src/containers/FoodsContainer.js
+++ src/containers/FoodsContainer.js
@@ -23,23 +23,27 @@ class FoodsContainer extends React.Component {
     }
 
     createFoodCards() {
-        let foods = this.props.foods.BrandedFoods
-        // carbs={(food.foodNutrients).find(food => food.nutrient.name.toLowerCase() === 'glucose')}
+        let foods = this.props.foods.FoundationFoods
+        // console.log(this.props.foods.FoundationFoods[0])
 
         if (foods && this.state.search.length !== 0) {
             foods = foods.filter(food => food.description.toLowerCase().includes(this.state.search.toLocaleLowerCase()))
-        } 
+        }
 
         if (foods) {
-            // console.log(foods.first)
-            // console.log(foods.map(food => (food.foodNutrients).find(food => food.nutrient.name.toLowerCase() == 'glucose')))
-            return foods.map(food => <FoodCard key={food.fdcId} description={food.description} carbs={food.labelNutrients.carbohydrates.value} servingSize={food.servingSize + food.servingSizeUnit}/>)
+            console.log((foods[0].foodNutrients.find(foodNutrient => foodNutrient.nutrient.name == "Carbohydrate, by difference")).amount)
+            console.log(foods)
+            return foods.map(food => <FoodCard 
+                key={food.fdcId} 
+                description={food.description} 
+                carbs={(food.foodNutrients.find(foodNutrient => foodNutrient.nutrient.name == "Carbohydrate, by difference")) ? (food.foodNutrients.find(foodNutrient => foodNutrient.nutrient.name == "Carbohydrate, by difference")).amount : 0}
+                />)
         }
     }
 
     render() {
         return (
-            <div>
+            <div className="">
                 <h2>Search:</h2>
                 <input type="text" vlaue={this.state.search} onChange={this.handleFormChange}></input><br></br><br></br>
                 {this.createFoodCards()}
