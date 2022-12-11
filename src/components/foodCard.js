97727e5e5c2bdb8bc118c622885b4fabc9db55cf
zaynegoering <zaynegoering@gmail.com>
2022-12-11 15:04:40 -0800

    add test sample food data

--- src/components/foodCard.js
+++ src/components/foodCard.js
@@ -2,12 +2,11 @@
 
 function FoodCard(props) {
 
-    console.log(props.carbs)
     
     return(
-        <div id={'fdc-id-' + props.fdcId}>
-            <h2>{(props.description).split(',')[0]}</h2>
-            {/* <h3>{props.description}</h3> */}
+        <div className="border rounded shadow mb-2 w-75 bg-dark" id={'fdc-id-' + props.fdcId}>
+            {/* <h2>{(props.description).split(',')[0]}</h2> */}
+            <h3>{props.description}</h3>
             <h3>Carbohydrates:</h3>
             <h3>{props.carbs}g</h3>
             <h3>Serving Size:</h3>
