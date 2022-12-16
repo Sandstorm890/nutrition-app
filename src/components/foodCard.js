120b32467a9e8b154554dcfc7e75a889b003697e
zaynegoering <zaynegoering@gmail.com>
2022-12-16 14:36:05 -0800

    updates formatting of food cards

--- src/components/foodCard.js
+++ src/components/foodCard.js
@@ -4,11 +4,10 @@ function FoodCard(props) {
 
     
     return(
-        <div className="border rounded shadow mb-2 w-75 bg-dark" id={'fdc-id-' + props.fdcId}>
-            {/* <h2>{(props.description).split(',')[0]}</h2> */}
-            <h3>{props.description}</h3>
-            <h3>Carbohydrates:</h3>
-            <h3>{props.carbs}g</h3>
+        <div className="border rounded shadow ml-4 mr-4 mb-3 bg-dark" id={'fdc-id-' + props.key}>
+            <h1>{(props.description).split(',')[0]}</h1>
+            <p>{props.description}</p>
+            <h2>Carbs: {props.carbs}g</h2>
             <h3>per {props.servingSize}</h3>
         </div>
     )
