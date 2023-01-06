b4114881112d3fc21198f0e26744c44719089843
zaynegoering <zaynegoering@gmail.com>
2023-01-06 13:14:06 -0800

    round carbs to int and change css for readability

--- src/components/foodCard.js
+++ src/components/foodCard.js
@@ -4,11 +4,11 @@ function FoodCard(props) {
 
     
     return(
-        <div className=" rounded shadow ml-4 mr-4 mb-3 pb-1 pt-1 bg-dark" id={'fdc-id-' + props.key}>
+        <div className="rounded shadow ml-4 mr-4 mb-3 pb-1 pt-1 bg-dark" id={'fdc-id-' + props.key}>
             <h1>{(props.description).split(',')[0]}</h1>
             <p>{props.description}</p>
-            <h2>Carbs: {props.carbs}g</h2>
-            <h3>per {props.servingSize}</h3>
+            <h2>Carbs: <span className="text-info">{Math.round(props.carbs)}g</span></h2>
+            <h3>per <span className="text-info">{props.servingSize}</span></h3>
         </div>
     )
 }
