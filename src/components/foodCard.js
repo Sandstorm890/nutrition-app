5be6dc7f452768d1a2e031cf461367d7d6637588
zaynegoering <zaynegoering@gmail.com>
2023-01-17 15:23:27 -0800

    remove split function for now

--- src/components/foodCard.js
+++ src/components/foodCard.js
@@ -7,7 +7,7 @@ function FoodCard(props) {
         <div className="card-body col-sm-4">
 
             <div className="rounded shadow pt-2 pb-2 bg-dark" id={'fdc-id-' + props.key}>
-                <h1>{(props.description).split(',')[0]}</h1>
+                <h1>{(props.description)}</h1>
                 <p>{props.description}</p>
                 <h2>Carbs: <span className="text-info">{Math.round(props.carbs)}g</span></h2>
                 <h3>per <span className="text-info">{props.servingSize}</span></h3>
