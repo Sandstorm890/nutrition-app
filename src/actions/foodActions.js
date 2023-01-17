b379f60084ed0b546c0fc6f9a4e03a7a659035f8
zaynegoering <zaynegoering@gmail.com>
2023-01-17 15:23:14 -0800

    implement string interpolation for urls

--- src/actions/foodActions.js
+++ src/actions/foodActions.js
@@ -1,12 +1,12 @@
-// const url = 'https://api.nal.usda.gov/fdc/v1/foods/search?api_key=QhydPZDu2L1Q1Faaw3ZO6bJ53WEu66LdBHIMfdDF&query='
-const url_all = 'https://api.nal.usda.gov/fdc/v1/foods/list?api_key=QhydPZDu2L1Q1Faaw3ZO6bJ53WEu66LdBHIMfdDF'
+const url = 'https://api.nal.usda.gov/fdc/v1/foods/search?api_key=QhydPZDu2L1Q1Faaw3ZO6bJ53WEu66LdBHIMfdDF&query='
+// const url_all = 'https://api.nal.usda.gov/fdc/v1/foods/list?api_key=QhydPZDu2L1Q1Faaw3ZO6bJ53WEu66LdBHIMfdDF'
 // import FoodData from '../resources/FoodData.json'
 export const setFoods = (foods) => ({type: 'GOT_FOODS', payload: foods})
 
 export const searchFoods = (term) => {
 
     return (dispatch) => {
-        fetch(`https://api.nal.usda.gov/fdc/v1/foods/list?api_key=QhydPZDu2L1Q1Faaw3ZO6bJ53WEu66LdBHIMfdDF&query=apple`)
+        fetch(`${url}${term}`)
         .then(r => r.json())
         .then(json => {
             dispatch(setFoods(json))
@@ -19,7 +19,7 @@ export const searchFoods = (term) => {
  export const getFoods = () => {
 
      return (dispatch) => {
-         fetch('https://api.nal.usda.gov/fdc/v1/foods/list?api_key=QhydPZDu2L1Q1Faaw3ZO6bJ53WEu66LdBHIMfdDF')
+         fetch('FoodData.json')
          .then(r => r.json())
          .then(json => {
              dispatch(setFoods(json))
