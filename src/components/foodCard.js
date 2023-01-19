78d37487afe338fc794de3f8dcf9bd90a6ec9593
zaynegoering <zaynegoering@gmail.com>
2023-01-19 15:05:33 -0800

    update UI

--- src/components/foodCard.js
+++ src/components/foodCard.js
@@ -4,11 +4,11 @@ function FoodCard(props) {
 
     
     return(
-        <div className="card-body col-sm-4">
+        <div className="card-body col-sm-12">
 
-            <div className="rounded shadow pt-2 pb-2 bg-dark" id={'fdc-id-' + props.key}>
+            <div className="rounded shadow pl-2 pt-2 pb-2 bg-dark" id={'fdc-id-' + props.key}>
                 <h1>{(props.description)}</h1>
-                <p>{props.description}</p>
+                <p>{props.brand}</p>
                 <h2>Carbs: <span className="text-info">{Math.round(props.carbs)}g</span></h2>
                 <h3>per <span className="text-info">{props.servingSize}</span></h3>
             </div>
