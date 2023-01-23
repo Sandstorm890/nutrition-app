d5ce5bee134f4a8b6f82fde1e2869f89bad784fd
zaynegoering <zaynegoering@gmail.com>
2023-01-23 13:56:33 -0800

    remove key prop

--- src/components/foodCard.js
+++ src/components/foodCard.js
@@ -6,7 +6,7 @@ function FoodCard(props) {
     return(
         <div className="card-body col-sm-12">
 
-            <div className="rounded shadow pl-2 pt-3 pb-2 bg-dark" id={'fdc-id-' + props.key}>
+            <div className="rounded shadow pl-2 pt-3 pb-2 bg-dark">
                 <h1>{(props.description)}</h1>
                 <p>{props.brand}</p>
                 <h2>Carbs: <span className="text-info">{Math.round(props.carbs)}g</span></h2>
