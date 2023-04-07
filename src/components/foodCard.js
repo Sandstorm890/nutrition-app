f0f9cef14e2e36c46bb408bf2f26b09d88bf81dc
zaynegoering <zaynegoering@gmail.com>
2023-04-07 13:47:21 -0700

    add category to food cards

--- src/components/foodCard.js
+++ src/components/foodCard.js
@@ -9,6 +9,7 @@ function FoodCard(props) {
             <div className="rounded shadow pl-2 pt-3 pb-2 bg-dark">
                 <h1>{(props.description)}</h1>
                 <p>{props.brand}</p>
+                <p>{props.category}</p>
                 <h2>Carbs: <span className="text-info">{Math.round(props.carbs)}g</span></h2>
                 <h3>per <span className="text-info">{props.servingSize}</span></h3>
             </div>
