7f78c4726cfd6d33ffe50436cfc911cdbb1632a1
zaynegoering <zaynegoering@gmail.com>
2023-04-07 13:46:16 -0700

    change color scheme

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header bg-secondary">
+      <header className="App-header">
         <FoodsContainer />
       </header>
     </div>
